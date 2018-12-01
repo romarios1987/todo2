966908c71f2ccfb9276a8c0254e01bf67689965e
romarios1987 <romios047@gmail.com>
2018-12-02 00:48:01 +0200

    Updates

---
+++ precache-manifest.7792bae22a45ea7ba9b869b24b91d51e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ff700ad10054b1075c2",
+    "url": "/todo2/static/js/runtime~main.7ff700ad.js"
+  },
+  {
+    "revision": "619627ab31f8b61a53b8",
+    "url": "/todo2/static/js/main.619627ab.chunk.js"
+  },
+  {
+    "revision": "25f751462d3ab51c1f13",
+    "url": "/todo2/static/js/1.25f75146.chunk.js"
+  },
+  {
+    "revision": "619627ab31f8b61a53b8",
+    "url": "/todo2/static/css/main.21322f4c.chunk.css"
+  },
+  {
+    "revision": "25f751462d3ab51c1f13",
+    "url": "/todo2/static/css/1.f9c59f7f.chunk.css"
+  },
+  {
+    "revision": "4c323b00e6cffb8f1b0a1bdbf45f0bcc",
+    "url": "/todo2/index.html"
+  }
+];
