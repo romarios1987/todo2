0ad6cd05674057deea17ab772430086161131d04
romarios1987 <romios047@gmail.com>
2020-04-06 12:46:37 +0300

    Updates

---
+++ precache-manifest.33f98951156474330eea65d85467475a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58dcc0fe43ef27e49f04225a63913c12",
+    "url": "/todo2/index.html"
+  },
+  {
+    "revision": "eb8fc88018a3d83d0781",
+    "url": "/todo2/static/css/2.e796649f.chunk.css"
+  },
+  {
+    "revision": "2bbe087159c00bd0c9ee",
+    "url": "/todo2/static/css/main.ca0821c4.chunk.css"
+  },
+  {
+    "revision": "eb8fc88018a3d83d0781",
+    "url": "/todo2/static/js/2.9a0beaef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo2/static/js/2.9a0beaef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbe087159c00bd0c9ee",
+    "url": "/todo2/static/js/main.d5ada171.chunk.js"
+  },
+  {
+    "revision": "1f9812ddc8bc0f789699",
+    "url": "/todo2/static/js/runtime-main.78f986ee.js"
+  }
+]);
